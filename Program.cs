3a0cca01d59edb47c33029d7f1113be24722b51e
iWildGuns <hrdr1995@gmail.com>
2025-09-18 03:31:06 -0300

    Terminando, faltaria el diagrama

--- Program.cs
+++ Program.cs
@@ -8,10 +8,30 @@ namespace Ejercicio05
         static void Main()
         {
             Console.WriteLine("Hello, World!");
-            DataProcess dp1 = new("Prueba", 13.2F, "Linux", "Entrando", "Saliendo");
+            DataProcess dp1 = new("Analisis", 13.02F, "Linux", "LEntry", "LOut");
+            DataProcess dp2 = new("Desarrollo", 3.12F, "Windows", "WEntry", "WOut");
+            DataProcess dp3 = new("Deploy", 7.23F, "iOs", "iEntry", "iOut");
 
-            dp1.TurnEstado("Entrando", "Saliendo");
-            dp1.clonarDB();
+
+            dp1.TurnEstado("Entrando", "Saliendo"); /* Se solicita ejecutar el metodo TurnEstado() sin exito ya que no son correctos los parametros para la confirmacion */
+            dp1.clonarDB(); /* Output Confirmacion denegada */
+
+            DataProcess[] dpArray = new DataProcess[3];
+            dpArray[0] = dp1;
+            dpArray[1] = dp2;
+            dpArray[2] = dp3;
+
+            DataProcess[] dpFalse = new DataProcess[33];
+
+            dpArray[1].TurnEstado("WEntry", "WOut");
+            dpArray[1].VerStado();
+
+            Application app1 = new("Aion", 3.0F, "Windows", "C#", 19.1F, dpArray);
+            app1.TurnEstado(); /* Output Confirma tu acceso a la instancia */
+            app1.TurnEstado("JavaScript", 9.10F, dpFalse); /* Introduzco falsas credenciales en los parametros para probar la negacion del acceso */
+            app1.TurnEstado("C#", 19.1F, dpArray); /* Pruebo Acceso con credenciales validos */
+
+            //* ---- Finalizando este ejercico el codigo, por ahi se me hace mas facil hacer el diagrama xD ---- *//
 
         }
         public abstract class Servidor
@@ -29,7 +49,7 @@ namespace Ejercicio05
             }
             public void TurnEstado()
             {
-                System.Console.WriteLine("Confirma tu acceso a la instancia");
+                System.Console.WriteLine("Confirma tu acceso a la instancia...");
             }
             public void VerStado()
             {
@@ -40,7 +60,7 @@ namespace Ejercicio05
         {
             private string DatosOrigen;
             private string DatosFin;
-            public DataProcess(string nombre, float version, string sistemaoptvo, string datosOrigen, string datosFin) : base(nombre, version, sistemaoptvo)
+            public DataProcess(string nombre, float version, string sistemaOptvo, string datosOrigen, string datosFin) : base(nombre, version, sistemaOptvo)
             {
                 this.DatosOrigen = datosOrigen;
                 this.DatosFin = datosFin;
@@ -56,12 +76,9 @@ namespace Ejercicio05
                 if (this.Estado)
                 {
                     System.Console.WriteLine("Se ha levantado correctamente la instancia... \nPosee acceso a datos de Origen y Fin almacenados...");
+                    return;
                 }
-                else
-                {
-                    System.Console.WriteLine("Se ha dado de baja correctamente la instancia...");
-
-                }
+                System.Console.WriteLine("Se ha dado de baja correctamente la instancia...");
             }
             public void clonarDB()
             {
@@ -69,9 +86,32 @@ namespace Ejercicio05
                 System.Console.WriteLine("Clonando DB...");
             }
         }
-        public class Application
+        public class Application : Servidor
         {
-
+            private string Lenguaje;
+            private float LenguajeV;
+            private DataProcess[] DataBase;
+            public Application(string nombre, float version, string sistemaOptvo, string lenguaje, float lenguajeV, DataProcess[] dataBase) : base(nombre, version, sistemaOptvo)
+            {
+                this.Lenguaje = lenguaje;
+                this.LenguajeV = lenguajeV;
+                this.DataBase = dataBase;
+            }
+            public void TurnEstado(string lgj, float ldjV, DataProcess[] dBase)
+            {
+                if (lgj != this.Lenguaje || ldjV != this.LenguajeV || dBase != this.DataBase)
+                {
+                    System.Console.WriteLine("Confirmacion denegada...");
+                    return;
+                }
+                this.Estado = !this.Estado;
+                if (this.Estado)
+                {
+                    System.Console.WriteLine($"Se ha levantado correctamente la instancia... \nSe ha instalado correctamente el lenguaje {this.Lenguaje} en la version {this.LenguajeV}");
+                    return;
+                }
+                System.Console.WriteLine("Se ha dado de baja correctamente la instancia...");
+            }
         }
     }
 }
